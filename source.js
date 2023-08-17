7cc7394531581f17e31ca68077a67ec4752fb455
joejs92 <joejs_92@yahoo.com>
2023-08-17 03:53:29 -0400

    Dynamically name created items and enable hover function

--- source.js
+++ source.js
@@ -4,11 +4,12 @@ let gridSize = userInput;
 function createGrid(gridSize) {
     while (gridSize > 0){
         const row = document.createElement('div');
-        row.id = "row";
+        row.className = "row";
         let width = userInput;
         while (width > 0){
             const column = document.createElement('div');
-            column.id = "grid";
+            column.className = "grid";
+            column.id = `grid${gridSize}${width}`;
             row.appendChild(column);
             width --;
         }
@@ -17,4 +18,15 @@ function createGrid(gridSize) {
     }
 }
 
+function changeColor(){
+    let thisID = this.id;
+    let change = document.getElementById(thisID);
+    change.style.backgroundColor = "black";
+}
+
 createGrid(gridSize);
+
+let btns = document.querySelectorAll('.grid');
+for (let i=0; i< btns.length; i++) {
+    btns[i].addEventListener('mouseover',changeColor);
+}
